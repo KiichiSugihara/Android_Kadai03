c84f5b2c2c852cdf4569cfeebbb9de0962df983c
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2020-05-17 09:59:02 +0900

    copy kadai02

--- app/src/main/java/com/example/kadai03/MainActivity.java
+++ app/src/main/java/com/example/kadai03/MainActivity.java
@@ -22,3 +22,60 @@ public class MainActivity extends AppCompatActivity {
         textView.setText( message);
     }
 }
+
+//package com.example.kadai03;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//import  android.os.Bundle;
+//import android.view.View;
+//import android.widget.EditText;
+//import android.widget.TextView;
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+//
+//    public void sendMessage(View view){
+//        EditText editTextA =(EditText) findViewById(R.id.edit_message_a);
+//        EditText editTextB =(EditText) findViewById(R.id.edit_message_b);
+//        TextView textView = (TextView) findViewById(R.id.view_message);
+//        String message = String.valueOf(Integer.parseInt(editTextA.getText().toString()) + Integer.parseInt(editTextB.getText().toString()));
+//        textView.setText( message);
+//    }
+////    public void plusNumber(View view){
+////        EditText editTextA =(EditText) findViewById(R.id.edit_message_a);
+////        EditText editTextB =(EditText) findViewById(R.id.edit_message_b);
+////        TextView textView = (TextView) findViewById(R.id.view_message);
+////        String message = String.valueOf(Integer.parseInt(editTextA.getText().toString()) + Integer.parseInt(editTextB.getText().toString()));
+////        textView.setText( message);
+////    }
+////    public void minusNumber(View view){
+////        EditText editTextA =(EditText) findViewById(R.id.edit_message_a);
+////        EditText editTextB =(EditText) findViewById(R.id.edit_message_b);
+////        TextView textView = (TextView) findViewById(R.id.view_message);
+////        String message = String.valueOf(Integer.parseInt(editTextA.getText().toString()) - Integer.parseInt(editTextB.getText().toString()));
+////        textView.setText( message);
+////    }
+////    public void multiplyNumber(View view){
+////        EditText editTextA =(EditText) findViewById(R.id.edit_message_a);
+////        EditText editTextB =(EditText) findViewById(R.id.edit_message_b);
+////        TextView textView = (TextView) findViewById(R.id.view_message);
+////        String message = String.valueOf(Integer.parseInt(editTextA.getText().toString()) * Integer.parseInt(editTextB.getText().toString()));
+////        textView.setText( message);
+////    }
+////    public void divideNumber(View view){
+////        EditText editTextA =(EditText) findViewById(R.id.edit_message_a);
+////        EditText editTextB =(EditText) findViewById(R.id.edit_message_b);
+////        TextView textView = (TextView) findViewById(R.id.view_message);
+////        String message = String.valueOf(Integer.parseInt(editTextA.getText().toString()) / Integer.parseInt(editTextB.getText().toString()));
+////        textView.setText( message);
+////    }
+////    public void clearNumber(View view){
+////        EditText editTextA =(EditText) findViewById(R.id.edit_message_a);
+////        EditText editTextB =(EditText) findViewById(R.id.edit_message_b);
+////    }
+//}
